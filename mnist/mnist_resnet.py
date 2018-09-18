77a52df03fd94730b21f54e87d485296cab37765
xinyu zhu <xyzhu@gmx.de>
2018-09-18 22:12:40 +0200

    - resnet 2

--- mnist/mnist_resnet.py
+++ mnist/mnist_resnet.py
@@ -3,10 +3,13 @@ import numpy as np
 from keras.utils import to_categorical
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+#from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
-from resnet import Residual
+
+
+from model_provider import *
 
 import tensorflow as tf
 config = tf.ConfigProto()
@@ -26,30 +29,9 @@ batch_size = 100
 nb_classes = 10
 
 # Model
-input_var = Input(shape=(img_rows, img_cols, 1))
-
-conv1 = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                      border_mode='same', activation='relu')(input_var)
-# conv1 = MaxPooling2D(pool_size=pool_size)(conv1)
-conv2 = Convolution2D(8, kernel_size[0], kernel_size[1],
-                      border_mode='same', activation='relu')(conv1)
-
-resnet = conv2
-for _ in range(10):
-    resnet = Residual(Convolution2D(8, kernel_size[0], kernel_size[1],
-                                  border_mode='same'))(resnet)
-    resnet = Activation('relu')(resnet)
-
-mxpool = MaxPooling2D(pool_size=pool_size)(resnet)
-flat = Flatten()(mxpool)
-dropout = Dropout(0.5)(flat)
-softmax = Dense(nb_classes, activation='softmax')(dropout)
-
-model = Model(input=[input_var], output=[softmax])
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy']) 
-
+model = make_resnet()
+#model.summary()
+#exit()
 
 # read data
 data, meta = arff.loadarff(filepath_train)
@@ -65,9 +47,6 @@ X_test = np.delete(testdataarray, 784, 1) / 255
 X_test = X_test.reshape(-1, img_rows, img_cols, 1)
 y_test = testdataarray[:, 784]
 y_test = to_categorical(y_test, 10)
-
-#X_50K = X_train[:50000]
-#y_50K = y_train[:50000]
 
 #model.fit(X_50K, y_50K, batch_size=100, epochs=20)
 model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=batch_size, epochs=20)
