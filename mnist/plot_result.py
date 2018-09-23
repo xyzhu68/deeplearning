8b9c3ff7f4b06463a7d17805384cf88b001f1df4
xinyu zhu <xyzhu@gmx.de>
2018-09-23 16:31:41 +0200

    - plot result of appear resnet

--- mnist/plot_result.py
+++ mnist/plot_result.py
@@ -30,19 +30,44 @@ plt.legend()
 plt.show()
 """
 
-data = np.load(Path("../DecisionTree/mnist_hoeffding.npz"))
+# data = np.load(Path("../DecisionTree/mnist_hoeffding.npz"))
 
-indices = data["indices"]
-accArray = data["acc"]
-accArray_E = data["acc_E"]
+# indices = data["indices"]
+# accArray = data["acc"]
+# accArray_E = data["acc_E"]
 
 
-# result of accuracy
-plt.plot(indices, accArray, label="acc patching clf")
-plt.plot(indices, accArray_E, label="acc error clf")
-plt.title("Accuracy")
-plt.ylabel("Accuracy")
-plt.xlabel("Batch")
-plt.legend()
-plt.show()
+# # result of accuracy
+# plt.plot(indices, accArray, label="acc patching clf")
+# plt.plot(indices, accArray_E, label="acc error clf")
+# plt.title("Accuracy")
+# plt.ylabel("Accuracy")
+# plt.xlabel("Batch")
+# plt.legend()
+# plt.show()
 
+def get_average_acc(file, acc_key):
+    cp = 20
+    data = np.load(file)
+    accArray = data[acc_key]
+    accArray = accArray[cp:]
+    return np.mean(accArray)
+
+def show_attach_result(clf, label):
+    accList = []
+    #accList2 = []
+    for i in range(7):
+        fileName = "mnist_drift_appear_resnet_{0}.npz".format(i)
+        accList.append(get_average_acc(fileName, clf))
+        #accList2.append(get_average_acc(fileName, clf2))
+
+    plt.plot(range(7), accList, label=label)
+    #plt.plot(range(7), accList2, label=label2)
+    plt.xlabel("frozen layers")
+    plt.ylabel("accuracy")
+    plt.legend()
+    plt.show()
+
+#show_attach_result("acc", "Patching Clf Accuracy")
+show_attach_result("acc_E", "Error Clf Accuracy")
+#show_attach_result("acc", "Patching Clf Accuracy", "acc_E", "Error Clf Accuracy")
